fcc256b4292b64a39f6e925d31593873ecf2ccd1
林世荣 <hdsmtiger@gmail.com>
2017-05-08 17:14:42 +0800

    ssh action杀死父进程同时杀死它下属的所有子进程

--- core/src/main/java/org/apache/oozie/action/ssh/SshActionExecutor.java
+++ core/src/main/java/org/apache/oozie/action/ssh/SshActionExecutor.java
@@ -171,7 +171,10 @@ public class SshActionExecutor extends ActionExecutor {
     @Override
     public void kill(Context context, WorkflowAction action) throws ActionExecutorException {
         //Kill process group in case sub process is isolated.
-        String command = "ssh " + action.getTrackerUri() + " kill -KILL -- -"+ action.getExternalId();
+        XLog log = XLog.getLog(getClass());
+        String command = "ssh " + action.getTrackerUri() + " kill -KILL -- -$(ps xao pid,pgid | grep "
+                + action.getExternalId() + " | cut -d ' ' -f2)";
+        log.info("Killing ssh job using command " + command);
         int returnValue = getReturnValue(command);
         if (returnValue != 0) {
             throw new ActionExecutorException(ActionExecutorException.ErrorType.ERROR, "FAILED_TO_KILL", XLog.format(
